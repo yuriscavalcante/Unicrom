4c437e10f1f6847040dee6debfa8c4719dd699e2
Yuri <yuri.scavalcante@outlook.com>
2022-03-20 18:07:03 -0300

    Home aprimorado

--- app/src/main/java/com/example/unicrom/activity/Home.java
+++ app/src/main/java/com/example/unicrom/activity/Home.java
@@ -1,81 +1,98 @@
 package com.example.unicrom.activity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.unicrom.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
     private TextView userName, userMat, academico;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     String userId;
     boolean visible = true;
-    ConstraintLayout expanded, expandedAdm;
+    RecyclerView recyclerView;
+    private StorageReference mStorageReference;
+    //List<Opcoes> opcoesList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         inicialize();
+        //recyclerView = findViewById(R.id.recycleView);
+        //recyclerView.setHasFixedSize(true);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("foto/2NoPxvnjAVapRQkJVO6PMlN3JYN2.jpeg");
+        try {
+            final File localFile = File.createTempFile("2NoPxvnjAVapRQkJVO6PMlN3JYN2","jpeg");
+            mStorageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(Home.this,"Tudo Certo", Toast.LENGTH_SHORT).show();
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    ((ImageView)findViewById(R.id.imageView2)).setImageBitmap(bitmap);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(Home.this,"Erro", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         DocumentReference documentReference = db.collection("aluno").document(userId);
         documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
                 if(documentSnapshot!=null){
-                    userName.setText(documentSnapshot.getString("nome"));
-                    userMat.setText(documentSnapshot.getString("matricula"));
+                    userName.setText("Olá "+documentSnapshot.getString("nome"));
+                    userMat.setText("Matricula: "+documentSnapshot.getString("matricula"));
                 }
 
             }
         });
-        expanded.setVisibility(View.GONE);
-        expandedAdm.setVisibility(View.GONE);
-    }
-    public void setVisible(View view){
-        //expanded.setVisibility(visible ? View.VISIBLE: View.GONE);
-        if(visible == true){
-            expanded.setVisibility(View.GONE);
-            visible = false;
-        }else{
-            expanded.setVisibility(View.VISIBLE);
-            visible = true;
-        }
-    }
 
-    public void setVisibleAdm(View view){
-        //expanded.setVisibility(visible ? View.VISIBLE: View.GONE);
-        if(visible == true){
-            expandedAdm.setVisibility(View.GONE);
-            visible = false;
-        }else{
-            expandedAdm.setVisibility(View.VISIBLE);
-            visible = true;
-        }
     }
 
     private void inicialize(){
-        academico = findViewById(R.id.academicoView);
         userName = findViewById(R.id.textUserName);
         userMat = findViewById(R.id.textUserMat);
-        expanded = findViewById(R.id.expandableLayout);
-        expandedAdm = findViewById(R.id.expandableLayoutAdm);
     }
+/*
+    private void initData() {
+        opcoesList = new ArrayList<>();
+
+    }*/
 }

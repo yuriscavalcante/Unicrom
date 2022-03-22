987084ded1b3bde1e71490277c1daa8752b8714a
Yuri <yuri.scavalcante@outlook.com>
2022-03-22 20:07:55 -0300

    Home aprimorado

--- app/src/main/java/com/example/unicrom/activity/Home.java
+++ app/src/main/java/com/example/unicrom/activity/Home.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.unicrom.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -35,33 +36,32 @@ public class Home extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     String userId;
     boolean visible = true;
+    String url = "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D1843890325876754&imgrefurl=https%3A%2F%2Fpt-br.facebook.com%2Ffernandoportelapessoa%2F&tbnid=v8P_Fr4GjYNyUM&vet=12ahUKEwi3zbfKwNf2AhXjHLkGHb85AEYQMygDegUIARDcAQ..i&docid=bqhuRmcCfNwaMM&w=1080&h=1080&q=pessoa&ved=2ahUKEwi3zbfKwNf2AhXjHLkGHb85AEYQMygDegUIARDcAQ";
     RecyclerView recyclerView;
     private StorageReference mStorageReference;
+    //private ImageView imageView;
     //List<Opcoes> opcoesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        inicialize();
+        setContentView(R.layout.teste);
+        //inicialize();
+        //userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("/gs://unicrom-f4a54.appspot.com/foto/2NoPxvnjAVapRQkJVO6PMlN3JYN2/avatar.png");
         //recyclerView = findViewById(R.id.recycleView);
         //recyclerView.setHasFixedSize(true);
         //recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        mStorageReference = FirebaseStorage.getInstance().getReference().child("foto/2NoPxvnjAVapRQkJVO6PMlN3JYN2.jpeg");
         try {
-            final File localFile = File.createTempFile("2NoPxvnjAVapRQkJVO6PMlN3JYN2","jpeg");
-            mStorageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            final File localFile = File.createTempFile("avatar","png");
+            mStorageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
                     Toast.makeText(Home.this,"Tudo Certo", Toast.LENGTH_SHORT).show();
                     Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                    ((ImageView)findViewById(R.id.imageView2)).setImageBitmap(bitmap);
+                    ((ImageView)findViewById(R.id.logoView)).setImageBitmap(bitmap);
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -72,6 +72,13 @@ public class Home extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
         DocumentReference documentReference = db.collection("aluno").document(userId);
         documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
@@ -84,11 +91,16 @@ public class Home extends AppCompatActivity {
             }
         });
 
+ /*       Glide.with(Home.this)
+                .load(url)
+                .centerCrop()
+                .placeholder(R.drawable.square_placeholder).into(imageView);*/
     }
 
     private void inicialize(){
         userName = findViewById(R.id.textUserName);
         userMat = findViewById(R.id.textUserMat);
+        //imageView = findViewById(R.id.avatarView);
     }
 /*
     private void initData() {
